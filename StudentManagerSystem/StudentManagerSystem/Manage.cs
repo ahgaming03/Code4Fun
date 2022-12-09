0dada5554ce661aafd949cd5efd4d8e991c3a5ca
ahgaming03 <85728869+ahgaming03@users.noreply.github.com>
2022-12-09 11:55:17 +0700

    Add files via upload

---
+++ StudentManagerSystem/StudentManagerSystem/Manage.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementProgram
+{
+    internal class Manage
+    {
+        private List<StudentInfo> StudentList = null;
+
+
+        public Manage()
+        {
+            StudentList = new List<StudentInfo>();
+        }
+
+        /*
+         * Generate ID for each student
+         */
+        private int GenerateID()
+        {
+            int max = 1;
+            if (StudentList != null && StudentList.Count > 0)
+            {
+                max = StudentList[0].ID;
+                foreach (StudentInfo studID in StudentList)
+                {
+                    if (max < studID.ID)
+                    {
+                        max = studID.ID;
+                    }
+                }
+                max++;
+            }
+            return max;
+        }
+
+        public int NumbersOfStudent()
+        {
+            int Count = 0;
+            if (StudentList != null)
+            {
+                Count = StudentList.Count;
+            }
+            return Count;
+        }
+
+        public void inputStudentInfo()
+        {
+            // Create a new student
+            StudentInfo inputInfo = new StudentInfo();
+            inputInfo.ID = GenerateID();
+            Console.Write("Name: ");
+            inputInfo.Name = Convert.ToString(Console.ReadLine());
+
+            Console.Write("Gender: ");
+            inputInfo.Gender = Convert.ToString(Console.ReadLine());
+
+            Console.Write("Age: ");
+            inputInfo.Age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Grade each subjects:");
+
+            Console.Write("Math: ");
+            inputInfo.Mathh = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Physical: ");
+            inputInfo.Physical = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Chemical: ");
+            inputInfo.Chemical = Convert.ToDouble(Console.ReadLine());
+
+            CalculateGPA(inputInfo);
+            Ranked(inputInfo);
+
+            StudentList.Add(inputInfo);
+        }
+
+        /*Update student information by ID
+         */
+        public void UpdateStudentInformation(int ID)
+        {
+            // Find student in student list
+            StudentInfo stud = FindByID(ID);
+            // If students exist, update student information
+            if (stud != null)
+            {
+                Console.WriteLine("Update this student information");
+                Console.WriteLine("(If you do not enter anything, do not update this function)");
+                Console.Write("Name: ");
+                string name = Convert.ToString(Console.ReadLine());
+                // If you do not enter anything, do not update name
+                if (name != null && name.Length > 0)
+                {
+                    stud.Name = name;
+                }
+
+                Console.Write("Gender: ");
+                // If you do not enter anything, do not update gender
+                string gender = Convert.ToString(Console.ReadLine());
+                if (gender != null && gender.Length > 0)
+                {
+                    stud.Gender = gender;
+                }
+
+                Console.Write("Age: ");
+                string ageStr = Convert.ToString(Console.ReadLine());
+                // If you do not enter anything, do not update age
+                if (ageStr != null && ageStr.Length > 0)
+                {
+                    stud.Age = Convert.ToInt32(ageStr);
+                }
+
+                Console.WriteLine("Grade each subjects:");
+
+                Console.Write("Math: ");
+                string mathStr = Convert.ToString(Console.ReadLine());
+                // If you do not enter anything, do not update math scores
+                if (mathStr != null && mathStr.Length > 0)
+                {
+                    stud.Mathh = Convert.ToDouble(mathStr);
+                }
+
+                Console.Write("Physical: ");
+                string physicalStr = Convert.ToString(Console.ReadLine());
+                // If you do not enter anything, do not update physical scores
+                if (physicalStr != null && physicalStr.Length > 0)
+                {
+                    stud.Physical = Convert.ToDouble(physicalStr);
+                }
+
+                Console.Write("Chemical: ");
+                string chemicalStr = Convert.ToString(Console.ReadLine());
+                // If you do not enter anything, do not update chemical scores
+                if (chemicalStr != null && chemicalStr.Length > 0)
+                {
+                    stud.Chemical = Convert.ToDouble(chemicalStr);
+                }
+
+                CalculateGPA(stud);
+                Ranked(stud);
+            }
+            else
+            {
+                Console.WriteLine("Student have ID = {0} does not exist.", ID);
+            }
+        }
+
+
+        /*
+         * Sort a student list according to the ID
+         */
+        public void SortByID()
+        {
+            StudentList.Sort(delegate (StudentInfo sv1, StudentInfo sv2)
+            {
+                return sv1.ID.CompareTo(sv2.ID);
+            });
+        }
+
+        /**
+         * Sort a student list according to the alphabet
+         */
+        public void SortByName()
+        {
+            StudentList.Sort(delegate (StudentInfo sv1, StudentInfo sv2)
+            {
+                return sv1.Name.CompareTo(sv2.Name);
+            });
+        }
+
+        /**
+         * Sort a student list according to the GPA
+         */
+        public void SortByGPA()
+        {
+            StudentList.Sort(delegate (StudentInfo sv1, StudentInfo sv2)
+            {
+                return sv1.GPA.CompareTo(sv2.GPA);
+            });
+        }
+
+        /**
+         * Find student by ID
+         * Show a student in list
+         */
+        public StudentInfo FindByID(int ID)
+        {
+            StudentInfo searchResult = null;
+            if (StudentList != null && StudentList.Count > 0)
+            {
+                foreach (StudentInfo studFind in StudentList)
+                {
+                    if (studFind.ID == ID)
+                    {
+                        searchResult = studFind;
+                    }
+                }
+            }
+            return searchResult;
+        }                                        
+
+        /**
+         * Find student by name
+         * Show a list of students
+         */
+        public List<StudentInfo> FindByName(String keyword)
+        {
+            List<StudentInfo> searchResult = new List<StudentInfo>();
+            if (StudentList != null && StudentList.Count > 0)
+            {
+                foreach (StudentInfo studFind in StudentList)
+                {
+                    if (studFind.Name.ToUpper().Contains(keyword.ToUpper()))
+                    {
+                        searchResult.Add(studFind);
+                    }
+                }
+            }
+            return searchResult;
+        }
+
+        /**
+         * Delete a student by ID
+         */
+        public bool isDeleteById(int ID)
+        {
+            bool Deleted = false;
+            // Find a student by ID
+            StudentInfo studDelete = FindByID(ID);
+            if (studDelete != null)
+            {
+                Deleted = StudentList.Remove(studDelete);
+            }
+            return Deleted;
+        }
+
+        /**
+         * Calculate GPA
+         */
+        private void CalculateGPA(StudentInfo stud)
+        {
+            double GPA = (stud.Mathh + stud.Physical + stud.Chemical) / 3;
+            stud.GPA = Math.Round(GPA, 2, MidpointRounding.AwayFromZero);
+        }
+
+        /**
+         * Ranked of student
+         */
+        private void Ranked(StudentInfo studRank)
+        {
+            if (studRank.GPA >= 8)
+            {
+                studRank.Rate = "Excellence";
+            }
+            else if (studRank.GPA >= 6.5)
+            {
+                studRank.Rate = "Great";
+            }
+            else if (studRank.GPA >= 5)
+            {
+                studRank.Rate = "Good";
+            }
+            else
+            {
+                studRank.Rate = "Bad";
+            }
+        }
+
+        /**
+         * Show student list
+         */
+        public void ShowStudentList(List<StudentInfo> studentList)
+        {
+            // Display column title
+            Console.WriteLine(" |{0, -5} | {1, -30} | {2, -7} | {3, 5} | {4, 6} | {5, 9} | {6, 5} | {7, 10} | {8, 10}|",
+                              "ID", "Name", "Gender", "Age", "Math", "Physical", "Chemical", "GPA", "Rank");
+            // Display student list
+            if (studentList != null && studentList.Count > 0)
+            {
+                foreach (StudentInfo stud in studentList)
+                {
+                    Console.WriteLine(" |{0, -5} | {1, -30} | {2, -7} | {3, 5} | {4, 6} | {5, 9} | {6, 5} | {7, 10} | {8, 10}|",
+                                      stud.ID, stud.Name, stud.Gender, stud.Age, stud.Mathh, stud.Physical, stud.Chemical,
+                                      stud.GPA, stud.Rate);
+                }
+            }
+            Console.WriteLine();
+        }
+
+        /*
+         * Get a student list
+         */
+        public List<StudentInfo> getStudentList()
+        {
+            return StudentList;
+        }
+    }
+
+}
+
---
+++ StudentManagerSystem/StudentManagerSystem/Program.cs
@@ -0,0 +1,141 @@
+﻿using StudentManagementProgram;
+
+Manage stud = new Manage();
+
+while (true)
+{
+    Console.WriteLine("\nSTUDENT MANAGEMENT PROGRAM");
+    Console.WriteLine(" ____________________________________________________________");
+    Console.WriteLine(" |                           MENU                           |");
+    Console.WriteLine(" |__________________________________________________________|");
+    Console.WriteLine(" | 1. | Add student.                                        |");
+    Console.WriteLine(" |____|_____________________________________________________|");
+    Console.WriteLine(" | 2. | Update student information by ID.                   |");
+    Console.WriteLine(" |____|_____________________________________________________|");
+    Console.WriteLine(" | 3. | Delete a student by ID.                             |");
+    Console.WriteLine(" |____|_____________________________________________________|");
+    Console.WriteLine(" | 4. | Find student by Name.                               |");
+    Console.WriteLine(" |____|_____________________________________________________|");
+    Console.WriteLine(" | 5. | Sort a student list by Grade Point Average (GPA).   |");
+    Console.WriteLine(" |____|_____________________________________________________|");
+    Console.WriteLine(" | 6. | Sort a student list by Name.                        |");
+    Console.WriteLine(" |____|_____________________________________________________|");
+    Console.WriteLine(" | 7. | Sort a student list by ID.                          |");
+    Console.WriteLine(" |____|_____________________________________________________|");
+    Console.WriteLine(" | 8. | Display student list.                               |");
+    Console.WriteLine(" |____|_____________________________________________________|");
+    Console.WriteLine(" | 0. | Exit.                                               |");
+    Console.WriteLine(" |____|_____________________________________________________|");
+    
+    Console.Write("\nYour selection: ");
+    int key = int.Parse(Console.ReadLine());
+    switch (key)
+    {
+        case 1:
+            Console.WriteLine("\n1. Add student.");
+            stud.inputStudentInfo();
+            Console.WriteLine("\nCompleted add new student!");
+            break;
+        case 2:
+            if (stud.NumbersOfStudent() > 0)
+            {
+                int id;
+                Console.WriteLine("\n2. Update student information. ");
+                Console.Write("\nEnter ID: ");
+                id = int.Parse(Console.ReadLine());
+                stud.UpdateStudentInformation(id);
+            }
+            else
+            {
+                Console.WriteLine("\nList of empty students!");
+            }
+            break;
+        case 3:
+            if (stud.NumbersOfStudent() > 0)
+            {
+                int id;
+                Console.WriteLine("\n3. Delete a student.");
+                Console.Write("\nEnter ID: ");
+                id = int.Parse(Console.ReadLine());
+                if (stud.isDeleteById(id))
+                {
+                    Console.WriteLine("\nStudent have id = {0} is deleted.", id);
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nList of empty students!");
+            }
+            break;
+        case 4:
+            if (stud.NumbersOfStudent() > 0)
+            {
+                Console.WriteLine("\n4. Find student by ten.");
+                Console.Write("\nEnter the name to find: ");
+                string name = Console.ReadLine();
+                List<StudentInfo> searchResult = stud.FindByName(name);
+                stud.ShowStudentList(searchResult);
+            }
+            else
+            {
+                Console.WriteLine("\nList of empty students!");
+            }
+            break;
+        case 5:
+            if (stud.NumbersOfStudent() > 0)
+            {
+                Console.WriteLine("\n5. Sort a student list by Grade Point Average (GPA).");
+                stud.SortByGPA();
+                stud.ShowStudentList(stud.getStudentList());
+            }
+            else
+            {
+                Console.WriteLine("\nList of empty students!");
+            }
+            break;
+        case 6:
+            if (stud.NumbersOfStudent() > 0)
+            {
+                Console.WriteLine("\n6. Sort a student list by Name.");
+                stud.SortByName();
+                stud.ShowStudentList(stud.getStudentList());
+            }
+            else
+            {
+                Console.WriteLine("\nList of empty students!");
+            }
+            break;
+        case 7:
+            if (stud.NumbersOfStudent() > 0)
+            {
+                Console.WriteLine("\n7. Sort a student list by ID.");
+                stud.SortByID();
+                stud.ShowStudentList(stud.getStudentList());
+            }
+            else
+            {
+                Console.WriteLine("\nList of empty students!");
+            }
+            break;
+        case 8:
+            if (stud.NumbersOfStudent() > 0)
+            {
+                Console.WriteLine("\n8. Display student list.");
+                stud.ShowStudentList(stud.getStudentList());
+            }
+            else
+            {
+                Console.WriteLine("\nList of empty students!");
+            }
+            break;
+        case 0:
+            Console.WriteLine("\nYou choose exit program!\nGOODBYE");
+            return;
+        default:
+            Console.WriteLine("\nThere is no this option!");
+            Console.WriteLine("\nPlease choose option in menu.");
+            break;
+    }
+}
+
+

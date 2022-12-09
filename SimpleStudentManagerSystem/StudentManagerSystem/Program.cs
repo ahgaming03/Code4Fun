3ff8ab167397649dfc3685919409c48eeb2a98ad
ahgaming03 <85728869+ahgaming03@users.noreply.github.com>
2022-12-09 16:32:12 +0700

    Add files via upload

---
+++ SimpleStudentManagerSystem/StudentManagerSystem/Program.cs
@@ -0,0 +1,62 @@
+﻿using SimpleStudentManagementProgram;
+
+Option stud = new Option();
+
+while (true)
+{
+    Console.WriteLine("\nSTUDENT MANAGEMENT PROGRAM");
+    Console.WriteLine(" ____________________________________________________________");
+    Console.WriteLine(" |                           MENU                           |");
+    Console.WriteLine(" |__________________________________________________________|");
+    Console.WriteLine(" | 1. | Add student.                                        |");
+    Console.WriteLine(" |____|_____________________________________________________|");
+    Console.WriteLine(" | 2. | Sort a student list by Grade Point Average (GPA).   |");
+    Console.WriteLine(" |____|_____________________________________________________|");
+    Console.WriteLine(" | 3. | Display student list.                               |");
+    Console.WriteLine(" |____|_____________________________________________________|");
+    Console.WriteLine(" | 0. | Exit.                                               |");
+    Console.WriteLine(" |____|_____________________________________________________|");
+    
+    Console.Write("\nYour selection: ");
+    int key = int.Parse(Console.ReadLine());
+    switch (key)
+    {
+        case 1:
+            Console.WriteLine("\n1. Add student.");
+            stud.inputStudentInfo();
+            Console.WriteLine("\nCompleted add new student!");
+            break;
+        case 2:
+            if (stud.NumbersOfStudent() > 0)
+            {
+                Console.WriteLine("\n2. Sort a student list by Grade Point Average (GPA).");
+                stud.SortByGPA();
+                stud.DisplayStudentList(stud.getStudentList());
+            }
+            else
+            {
+                Console.WriteLine("\nList of empty students!");
+            }
+            break;
+        case 3:
+            if (stud.NumbersOfStudent() > 0)
+            {
+                Console.WriteLine("\n3. Display student list.");
+                stud.DisplayStudentList(stud.getStudentList());
+            }
+            else
+            {
+                Console.WriteLine("\nList of empty students!");
+            }
+            break;
+        case 0:
+            Console.WriteLine("\nYou choose exit program!\nGOODBYE");
+            return;
+        default:
+            Console.WriteLine("\nThere is no this option!");
+            Console.WriteLine("\nPlease choose option in menu.");
+            break;
+    }
+}
+
+
---
+++ SimpleStudentManagerSystem/StudentManagerSystem/StudentInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleStudentManagementProgram
+{
+    internal class StudentInfo
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public double Mathh { get; set; }
+        public double Physical { get; set; }
+        public double Chemical { get; set; }
+        public double GPA { get; set; }
+    }
+}
